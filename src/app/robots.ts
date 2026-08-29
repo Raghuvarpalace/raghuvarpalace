1504ab6a34f8eef1195848acc3a0fab2d9c0fd00
Raghuvarpalace <reelrampo@gmail.com>
2026-08-30 00:23:17 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,10 @@
+import type { MetadataRoute } from "next";
+import { hotelConfig } from "@/lib/hotel-config";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: `${hotelConfig.siteUrl.replace(/\/$/, "")}/sitemap.xml`,
+  };
+}
 
